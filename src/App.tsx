1c3183d0001bef5c0e5a3559ef04b78b3a772ba6
Anshul <anshul@belgiumwebnet.com>
2026-01-06 16:50:36 +0530

    chnages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 
+import SignIn from "./_auth/forms/SignIn"
+import Signup from "./_auth/forms/Signup"
 import "./global.css"
+import {Routes , Route} from "react-router-dom"
+import { Home } from "./root/pages"
+
 
 
 function App() {
   return (
-    <h1 className="text-3xl underline font-bold">
-      Hello World
-    </h1>
+   <main className="flex h-screen">
+     <Routes>
+      {/* public route */}
+      <Route path = "/sign-up" element = {<Signup/>}/>
+      <Route path = "/sign-in" element = {<SignIn/>}/>
+
+       {/* private route */}
+       <Route index element = {<Home/>}/>
+     </Routes>
+   </main>
   )
 }
 
