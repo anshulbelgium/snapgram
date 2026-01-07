691c186cc77c95c87ea7029300952e585ef6fd5a
Anshul Tamrakar <anshultamrakar487@gmail.com>
2026-01-07 23:11:08 +0530

    validations

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Form, FormControl , FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { validationFormSchema } from "@/lib/validations"
 
@@ -11,7 +11,10 @@ const SignupForm = () => {
   const form = useForm<z.infer<typeof validationFormSchema>>({
     resolver: zodResolver(validationFormSchema),
     defaultValues: {
+      name : "",
       username: "",
+      email : "",
+      password : ""
     },
   })
 
@@ -21,7 +24,24 @@ const SignupForm = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <div className="flex flex-col">
+        <img src="/assets/images/logo.svg" alt = "logo"/>
+        <h2>Log in to your account</h2>
+        <p>Welcome Back!Please enter your details.</p>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col w-full gap-5 mt-4">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input type = "text"  {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <FormField
           control={form.control}
           name="username"
@@ -29,17 +49,41 @@ const SignupForm = () => {
             <FormItem>
               <FormLabel>Username</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input type = "text" {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+         <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type = "text"  {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+         <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type = "password"  {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" type="submit">Submit</Button>
       </form>
+      </div>
     </Form>
   )
 }
--- src/lib/validations/index.ts
+++ src/lib/validations/index.ts
@@ -1,7 +1,16 @@
 import { z } from "zod"
 
 export const validationFormSchema = z.object({
+  name : z.string().min(2 , {
+    message : "Name must be at least 2 characters."
+  }),
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
+  email : z.email().min(2, {
+    message : "Enter a valid email"
+  }),
+  password : z.string().min(2 , {
+    message : "Password must be atleast 8 character long"
+  })
 })
